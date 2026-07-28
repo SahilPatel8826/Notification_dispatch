edfdae7e5bbb294cd255fb6de4e70c1d709c53df
SahilPatel8826 <sahilpatel8826@gmail.com>
2026-07-28 20:16:51 +0000

    Jul 29, 2026, 1:46 AM

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,15 +1,29 @@
 package queue
 
-import "notification_dispatch/internal/job"
+import (
+	"notification_dispatch/internal/job"
+	"sync"
+)
+
+type Status string
+
+const (
+	Pending Status = "PENDING"
+	Running Status = "RUNNING"
+	Done    Status = "DONE"
+	Failed  Status = "FAILED"
+)
 
 type Queue struct {
-	jobs chan job.Job
+	jobs   chan job.Job
+	status map[string]Status
+	mu     sync.RWMutex
 }
 
-func (q *Queue) Enqueue(job Job) {
+func (q *Queue) Enqueue(job job.Job) {
 	q.jobs <- job
 }
 
-func (q *Queue) Dequeue() Job {
+func (q *Queue) Dequeue() job.Job {
 	return <-q.jobs
 }
